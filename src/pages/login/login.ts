e2bb93e3913c79911b0d96ad3c967579be0a94e1
Maxim Verbovoy <vermaxik@gmail.com>
2017-04-12 21:02:56 +0300

    Init commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { TabsPage } from '../tabs/tabs';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {
+    this.navCtrl.setRoot(TabsPage);
+  }
+
+}
